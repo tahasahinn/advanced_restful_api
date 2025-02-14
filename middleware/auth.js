04ec171760c86d1ce2670b08e572c74e53383bf6
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-02-14 16:07:45 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ export const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split("")[1];
     let decodedData;
-s
+
     if (token) {
       decodedData = jwt.verify(token, process.env.SECRET_TOKEN);
       req.userId = decodedData?.id;
